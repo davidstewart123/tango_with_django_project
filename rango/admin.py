fae6546a070ffb988819153001d1bb2a574cd5d8
David Stewart <0407532s@ad.dcs.gla.ac.uk>
2018-01-27 19:23:38 +0000

    Chapter 6 complete

--- rango/admin.py
+++ rango/admin.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
 
 # Register your models here.
--- rango/views.py
+++ rango/views.py
@@ -4,16 +4,41 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    #construct a dictionary to pass to the template engine as its context.
-    #Note the key boldmessage is the same as {{ boldmessage }}in the template!
-    context_dict = {'boldmessage': "Crunchy, creamie, cookie, candy, cupcake!"}
+    #Query the database for a list of ALL categories and pages currently stored.
+    #Order the categories by no. likes and pages by no. views in descending order.
+    #Retrieve the top 5 only - or all if less than 5.
+    #Place the list in our context_dict dictionary
+    #This will be passed to the template engine
+    context_dict = {}
 
-    #Return a rendered response to send to the client.
-    #We make use of the shortcut function to make our lives easier.
-    #Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    category = Category.objects.order_by('-likes')[:5]
+
+    pages = Page.objects.order_by('-views')[:5]
+
+    context_dict['pages'] = pages
+
+    context_dict['categories'] = category
+
+    #Return the rendered response and sent it back!
+    return render(request, 'rango/index.html', context_dict, context_dict)
 def about(request):
-    #htmla = "Rango says here is the about page" + '<a href="/rango/">Index</a>'
     return render(request, 'rango/about.html')
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
